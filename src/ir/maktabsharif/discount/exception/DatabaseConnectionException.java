77c54f4e5d13c78fa89b1acdc09848334f9e9a00
MhN_AR <mahan85a@gmail.com>
2026-07-10 15:25:04 +0330

    feat: add DatabaseConnectionException

---
+++ src/ir/maktabsharif/discount/exception/DatabaseConnectionException.java
@@ -0,0 +1,7 @@
+package ir.maktabsharif.discount.exception;
+
+public class DatabaseConnectionException extends RuntimeException {
+    public DatabaseConnectionException(String message) {
+        super(message);
+    }
+}
