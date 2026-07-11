347f568ba2bf83ce066243b0644a8fff16513e6a
sajjad.omidvar <sajjad.omidvar1372@gmail.com>
2026-07-11 23:11:45 +0330

    refactor: findById method updated

--- src/ir/maktabsharif/discount/repository/CouponRepository.java
+++ src/ir/maktabsharif/discount/repository/CouponRepository.java
@@ -3,10 +3,11 @@ package ir.maktabsharif.discount.repository;
 import ir.maktabsharif.discount.model.Coupon;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CouponRepository {
     Long insert(Coupon coupon);
-    Coupon findById(Long id);
+    Optional<Coupon> findById(Long id);
     List<Coupon> findAll();
     boolean existCoupon(String code);
 }
